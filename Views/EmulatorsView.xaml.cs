ad795d6896c57e0fdb91a178fcc0718dc92be377
TokamiGankei <tokamigankei@hotmail.com>
2026-06-06 15:05:32 -0300

    fix: CS0246 remove SettingsViewModel reference from EmulatorsView

--- Views/EmulatorsView.xaml.cs
+++ Views/EmulatorsView.xaml.cs
@@ -1,26 +1,49 @@
+
 using System.Windows;
+
 using System.Windows.Controls;
 
+
+
 namespace GameSnapPlugin.Views
+
 {
+
     public partial class EmulatorsView : UserControl
+
     {
+
         public EmulatorsView()
+
         {
+
             InitializeComponent();
+
         }
 
+
+
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
+
         {
+
             if (sender is Button btn && btn.Tag is EmulatorProfile profile)
+
             {
-                var vm = DataContext as SettingsViewModel;
-                var path = vm?.BrowseForFolder();
+
+                var settings = DataContext as GameSnapSettings;
+
+                var path = settings?.BrowseForFolder();
+
                 if (path != null)
-                {
+
                     profile.CustomPath = path;
-                }
+
             }
+
         }
+
     }
+
 }
+
